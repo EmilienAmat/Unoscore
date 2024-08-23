b3ae88956246f276586b8858af967c47fee9e52c
Emilien AMATJALAL <58630319+EmilienAmat@users.noreply.github.com>
2024-08-23 12:30:30 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,27 @@
 self.addEventListener('install', event => {
-    event.waitUntil(
+  event.waitUntil(
       caches.open('uno-score-cache').then(cache => {
-        return cache.addAll([
-          './',
-          './index.html',
-          './style.css',
-          './script.js',
-          './LOGO.png',
-          './add.png',
-          './subtract.png',
-          './settingLogo.png',
-          './resetLogo.png'
-        ]);
+          return cache.addAll([
+              './index.html',
+              './style.css',
+              './script.js',
+              './LOGO.png',
+              './add.png',
+              './subtract.png',
+              './settingLogo.png',
+              './resetLogo.png',
+              './manifest.json',
+              './image/icon-192x192.png',
+              './image/icon-512x512.png'
+          ]);
       })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
       caches.match(event.request).then(response => {
-        return response || fetch(event.request);
+          return response || fetch(event.request);
       })
-    );
-  });
-  
-  
+  );
+});
